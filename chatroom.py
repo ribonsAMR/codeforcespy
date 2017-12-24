e68eb6d160c3fc8265efebc58bb9e71a61c59bfa
Amr Essam <amressam01@hotmail.com>
2017-12-24 22:57:08 +0200

    chatroom improvement

--- chatroom.py
+++ chatroom.py
@@ -1,14 +1,12 @@
 word = 'hello'
 inp = input()
 index = 0
-count = 0
 for i in inp:
 	if index == len(word):
 		break
 	if i == word[index]:
-		count += 1
 		index += 1
-if count == len(word):
+if index == len(word):
 	print("YES")
 else:
 	print("NO")
